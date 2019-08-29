d7da13a034cf11f2349b49779aa925041b365516
James Wood <woodjames275@gmail.com>
2019-08-29 16:00:11 -0700

    swift strings (incomplete)

---
+++ swift/strings.swift
@@ -0,0 +1,13 @@
+// An introduction to strings in Swift
+
+var movieName = "Ferris Bueller's Day Off"  // assignment using a string literal
+var movieQuote = """
+    Life move's pretty fast some times. \
+    If you don't look around every once in a while
+    you might miss it.
+"""                                         // assignment using a multi-line string
+
+// mutability
+
+var message = "The first part."
+message += " The next part."
