af90c3155e89b88c484c753b6600587ce1a52160
James Wood <woodjames275@gmail.com>
2019-09-04 20:53:07 -0700

    add c program structure file

---
+++ c/structure.c
@@ -0,0 +1,17 @@
+// This file shows the basic structure of a c file
+
+// 1) Header, these lines are handled by a preprocessor to replace lines prepended with '#'
+#include <stdio.h>
+
+// 2) Declare a main function 
+int main() {
+    // 3) declare variables
+    // 4) function body
+    printf("%s", "Hello World");
+    // 5) return statement, 0 typically represents a successfully terminated program
+    return 0;
+}
+
+// compile:
+// $ gcc -o compiled_program filename.c
+// $ ./compilted_program
